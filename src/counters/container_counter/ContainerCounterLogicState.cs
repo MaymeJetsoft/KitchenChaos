b640cccb3debdcadd93a0e09509b52dbe8976f80
MaymeJetsoft <mayme.jetsoft@proton.me>
2026-09-20 15:02:19 +0200

    reformat on saving C# files

--- src/counters/container_counter/ContainerCounterLogicState.cs
+++ src/counters/container_counter/ContainerCounterLogicState.cs
@@ -8,28 +8,28 @@ using Chickensoft.LogicBlocks;
 public partial record ContainerCounterLogicState : LogicBlockState,
   IGet<ContainerCounterLogicState.Input.Interact>
 {
-  public static class Input
-  {
-    public readonly record struct Interact(bool PlayerHasKitchenObject);
-  }
-
-  public static class Output
-  {
-    public readonly record struct SpawnRequested(KitchenObjectType Type);
-    public readonly record struct InteractionRejected;
-  }
-
-  public Type On(in Input.Interact input)
-  {
-    if (input.PlayerHasKitchenObject)
+    public static class Input
     {
-      Output(new Output.InteractionRejected());
-    }
-    else
-    {
-      Output(new Output.SpawnRequested(Get<ContainerCounterLogic.Data>().Type));
+        public readonly record struct Interact(bool PlayerHasKitchenObject);
     }
 
-    return ToSelf();
-  }
+    public static class Output
+    {
+        public readonly record struct SpawnRequested(KitchenObjectType Type);
+        public readonly record struct InteractionRejected;
+    }
+
+    public Type On(in Input.Interact input)
+    {
+        if (input.PlayerHasKitchenObject)
+        {
+            Output(new Output.InteractionRejected());
+        }
+        else
+        {
+            Output(new Output.SpawnRequested(Get<ContainerCounterLogic.Data>().Type));
+        }
+
+        return ToSelf();
+    }
 }
